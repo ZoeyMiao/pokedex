627ac49a488c1461e944615276aa15943d36125e
Zoey  Sun <zoeysun@Zoeys-MacBook-Pro.local>
2016-03-26 21:40:56 -0400

    Refresh the UI in the storyboard and get info

---
+++ pokedex/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Zoey  Sun on 3/26/16.
+//  Copyright © 2016 Zoey  Sun. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+//create a closure
+typealias DownLoadComplete = () -> ()
